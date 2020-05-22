3a58bc57cf349113ce34a95f658faa64854739d5
Murilo Henrique <murilomatias@alunos.utfpr.edu.br>
2020-05-21 22:51:42 -0300

    Game class is entirely static

--- pangolin/main.ts
+++ pangolin/main.ts
@@ -1,10 +1,7 @@
-import { Entity } from "./Entity";
 import { Tile } from "./Tile";
 import { Room } from "./Room";
-import tile1 from '../src/tiles/main.png';
 import { GraphicEntity } from "./GraphicEntity";
 import { PhysicEntity } from "./PhysicEntity";
-import { Sprite } from "./Sprite";
 import gameSets from '../src/main';
 
 export interface IGameConfig {
@@ -17,13 +14,21 @@ export class Game{
   private static keyUpEvent: KeyboardEvent;
   private static keyDownEvent: KeyboardEvent;
 
-  private pressedKeys: any;
-  private gameCanvas: HTMLCanvasElement;
-  private cachedTiles;
-  private currentRoom: Room;
-  private config: IGameConfig = {showCollisionBoxes: true};
+  private static pressedKeys: any;
+  private static gameCanvas: HTMLCanvasElement;
+  private static cachedTiles;
+  private static currentRoom: Room;
+  private static config: IGameConfig = {showCollisionBoxes: true};
 
   private constructor(){
+    
+  }
+
+  /**
+   * Initializes the game. Called outside Game class.
+   */
+  public static init(){
+    //COnfigure game canvas:
     this.gameCanvas = document.getElementById('game-canvas') as HTMLCanvasElement;
     this.gameCanvas.style.border = '1px solid black';
     this.gameCanvas.style.background = '#ccc';
@@ -34,13 +39,6 @@ export class Game{
     this.cachedTiles = gameSets.tiles;
     //Loads initial room:
     this.currentRoom = gameSets.configs.initialRoom;
-  }
-
-  /**
-   * Initializes the game. Called outside Game class.
-   */
-  public static init(){
-    this._instance = new Game();
     //Initializes the keyboard events listener:
     document.addEventListener('keyup', (e) => this.handleKeyupEvent(e));
     document.addEventListener('keydown', (e) => this.handleKeydownEvent(e));
@@ -49,7 +47,7 @@ export class Game{
   /**
    * Runs the current cycle. Called at setInterval() outside Game class every frame.
    */
-  public static _run(){
+  public static run(){
     this.moveEntities();
     this.runEntities();
     this.renderEntities();
@@ -60,19 +58,19 @@ export class Game{
    * refresh their _x, _y position in function of their _vSpeed and _hSpeed.
    */
   private static moveEntities(){
-    this._instance.currentRoom.entities.forEach(entity => {
+    this.currentRoom.entities.forEach(entity => {
       if(entity instanceof GraphicEntity || entity instanceof PhysicEntity){
         entity._refreshPosition();
       }
     })
-    this._instance.currentRoom._handleCollisions();
+    this.currentRoom._handleCollisions();
   }
 
   /**
    * Runs the run() of literally every entity of the current room at current cycle.
    */
   private static runEntities(){
-    this._instance.currentRoom.entities.forEach(entity => {
+    this.currentRoom.entities.forEach(entity => {
       entity.run();
     })
   }
@@ -82,13 +80,13 @@ export class Game{
    * their sprite/animation frame at current cycle.
    */
   private static renderEntities(){
-    const canvas = this._instance.gameCanvas;
+    const canvas = this.gameCanvas;
     const ctx = canvas.getContext('2d');
     ctx.imageSmoothingEnabled = false;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
     //Sorts entities by depth:
-    let sortedEntities = this._instance.currentRoom.getDrawableEntities()
+    let sortedEntities = this.currentRoom.getDrawableEntities()
     .sort((entityA, entityB) => entityA._get('_depth') - entityB._get('_depth'));
     //Iterate drawable entities and render them:
     sortedEntities.forEach((entity) =>{
@@ -110,7 +108,7 @@ export class Game{
         let dHeight = sprite.getYScale() * (flipVer ? -sHeight : sHeight);
         ctx.rotate(rotation * Math.PI / 180);
         ctx.drawImage(img, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
-        if(this._instance.config.showCollisionBoxes){
+        if(this.config.showCollisionBoxes){
           ctx.strokeStyle = '#FFFFFF';
           ctx.beginPath();
           let collider = entity.getCollisionMask().instance;
@@ -127,7 +125,7 @@ export class Game{
    * @param name The registered tile name.
    */
   public static getCachedTile(name: string): Tile{
-    return this._instance.cachedTiles[name];
+    return this.cachedTiles[name];
   }
 
   /**
@@ -135,7 +133,7 @@ export class Game{
    * @param key Key (ex: 'q', 'F5', '9', 'Shift')
    */
   public static isKeyPressed(key: string): boolean{
-    return this._instance.pressedKeys[key];
+    return this.pressedKeys[key];
   }
 
   /**
@@ -143,18 +141,18 @@ export class Game{
    * @param e Event
    */
   private static handleKeyupEvent(e: KeyboardEvent){
-    this._instance.pressedKeys[e.key] = false;
-    this._instance.currentRoom.entities.forEach(entity => entity.onKeyUp(e))
+    this.pressedKeys[e.key] = false;
+    this.currentRoom.entities.forEach(entity => entity.onKeyUp(e))
   }
   /**
    * Passes the keydown event to this.keyDownEvent so it may be pssed to entities.
    * @param e Event
    */
   private static handleKeydownEvent(e: KeyboardEvent){
-    this._instance.pressedKeys[e.key] = true;
-    this._instance.currentRoom.entities.forEach(entity => entity.onKeyDown(e))
+    this.pressedKeys[e.key] = true;
+    this.currentRoom.entities.forEach(entity => entity.onKeyDown(e))
   }
 }
 
 Game.init();
-setInterval((() => Game._run()), 20);
+setInterval((() => Game.run()), 20);
